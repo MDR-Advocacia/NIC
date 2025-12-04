a2b01ae253007e14a6d6cbe38c5bbd776af5f9df
Murilo Silva <murilo.silva.136@ufrn.edu.br>
2025-12-04 13:45:59 -0300

    Fix: Adiciona validação do campo Area no UserController para permitir salvamento

--- concilia-backend/src/app/Models/User.php
+++ concilia-backend/src/app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable
         'password',
         'role',
         // --- NOVOS CAMPOS ADICIONADOS ---
+        'area',
         'status',
         'phone',
         'last_login_at',
