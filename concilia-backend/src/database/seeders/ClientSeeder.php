3f34bab65fe89f61d98fa45f5af09e5718d6d6b3
Antônio Neto <antonio.souzneto@gmail.com>
2025-12-19 09:49:51 -0300

    Adicionando novas mudanças visuais

--- concilia-backend/src/database/seeders/ClientSeeder.php
+++ concilia-backend/src/database/seeders/ClientSeeder.php
@@ -16,15 +16,9 @@ class ClientSeeder extends Seeder
         // Lista apenas com os bancos que você quer
         $clients = [
             [
-                'name' => 'Banco do Brasil S.A.', // Necessário para testar Ourocap
+                'name' => 'Banco do Brasil S.A.', 
                 'email' => 'contato@bb.com.br',
                 'phone' => '0800 729 0001',
-                'status' => 'ativo',
-            ],
-            [
-                'name' => 'Livelo S.A.',          // Necessário para testar Pontos
-                'email' => 'juridico@livelo.com.br',
-                'phone' => '0800 000 0000',
                 'status' => 'ativo',
             ],
         ];
