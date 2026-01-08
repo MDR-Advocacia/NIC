62e7050566e0c00379a08c7751d06c3f5a815a68
Pedro Arthur Magalhães Alecrim <118025418+pedroalec@users.noreply.github.com>
2026-01-08 12:48:13 -0300

    Ajustando o arquivo para permitir o link de acesso do frontend
    Added server configuration to allow external connections and specify allowed hosts.

--- concilia-frontend/vite.config.js
+++ concilia-frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permite conexões externas ao container
+    port: 5173,
+    allowedHosts: [
+      'nic.mdradvocacia.com', // Adicione o seu domínio aqui
+      '.mdradvocacia.com'     // O ponto no início permite qualquer subdomínio
+    ]
+  }
 })
