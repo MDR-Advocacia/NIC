a68287ffe79ff9e9796296baccdd89126c06fa8c
Murilo Silva <murilo.silva.136@ufrn.edu.br>
2026-02-20 14:27:33 -0300

    fix: remove dump-autoload do build

--- concilia-backend/src/config/cors.php
+++ concilia-backend/src/config/cors.php
@@ -1,21 +1,12 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie, 'login''],
-
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
     'allowed_methods' => ['*'],
-
-    // Mude esta linha para aceitar qualquer origem (resolve o problema imediatamente)
     'allowed_origins' => [env('CORS_ALLOWED_ORIGINS', '*')],
-
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
-    // Como seu sistema usa Token (Bearer), deixe false para permitir o '*' acima.
     'supports_credentials' => true,
 ];
