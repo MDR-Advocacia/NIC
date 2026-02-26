844ebed7958717d3cf08d7ed767ea617f9d8b7f2
Murilo Silva <murilo.silva.136@ufrn.edu.br>
2026-02-26 08:38:48 -0300

    feat: connect inbox to chatwoot

--- concilia-backend/src/config/cors.php
+++ concilia-backend/src/config/cors.php
@@ -3,10 +3,11 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('CORS_ALLOWED_ORIGINS', '*')],
+    'allowed_origins' => [env('CORS_ALLOWED_ORIGINS', 'https://lab-nic.mdradvocacia.com')],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
 ];
+
