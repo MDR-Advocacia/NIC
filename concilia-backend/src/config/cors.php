2096c4ccaa39466c910150de8fdd19174e7b92c6
Murilo Silva <murilo.silva.136@ufrn.edu.br>
2026-03-03 15:41:00 -0300

    Feat: Adicionando nova const para enviar mensagem ao usuario do chatwoot pelo nic atraves da api de integracao

--- concilia-backend/src/config/cors.php
+++ concilia-backend/src/config/cors.php
@@ -3,7 +3,11 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [env('CORS_ALLOWED_ORIGINS', 'https://lab-nic.mdradvocacia.com')],
+    // 'allowed_origins' => [env('CORS_ALLOWED_ORIGINS', 'https://lab-nic.mdradvocacia.com')],
+    'allowed_origins' => [
+        'https://lab-nic.mdradvocacia.com',
+        'http://localhost:3000'
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
