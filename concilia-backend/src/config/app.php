5be9b4b8e5e3a01d407d51cd6e1708abeca7febf
Murilo Silva <murilo.silva.136@ufrn.edu.br>
2026-05-07 16:17:48 -0300

    Remove variaveis frontend sensiveis do compose

--- concilia-backend/src/config/app.php
+++ concilia-backend/src/config/app.php
@@ -53,7 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
-    'frontend_url' => env('APP_FRONTEND_URL', env('APP_URL', 'http://localhost')),
+    'frontend_url' => env('APP_FRONTEND_URL', env('FRONTEND_URL')),
 
     /*
     |--------------------------------------------------------------------------
